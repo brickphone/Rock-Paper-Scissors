dcf3971167c366e0de0c3658ce64ff0ee402d58c
brickphone <mattis.hutt@gmail.com>
2023-03-05 13:41:12 +0100

    working

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
 
-  if (computerChoice == 1) {
+  if (computerChoice == 0) {
     return ("Rock");
-  } else if (computerChoice == 2) {
+  } else if (computerChoice == 1) {
     return ("Paper");
-  } else if (computerChoice == 3) {
+  } else if (computerChoice == 2) {
     return ("Scissors");
   }
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
 
-console.log(getComputerChoice);
